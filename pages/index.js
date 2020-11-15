4f1286708e930c3b8cab24726ee47069080401b7
fcvlh <felipecarvalho4@hotmail.com>
2020-11-14 23:29:31 -0300

    Primeira atulização vercel

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,7 @@
 import { useState } from 'react';
 
 function Home() {
-    return (
-        <div>
-            <h1>Home 3</h1>
-            <Contador />
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador,SetContador]=useState(1);
-
-    function adicionarContador() {
-        SetContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+    return <div>Home</div>
 }
 
 export default Home
