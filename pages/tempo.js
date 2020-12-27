c6c35227745b7e6407a148c438afbf6a1d5f7569
fcvlh <felipecarvalho4@hotmail.com>
2020-12-27 01:45:57 -0300

    Log GetStaticProps

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,4 +1,5 @@
 function Tempo(props) {
+    console.log('> Passando pelo FrontEnd;')
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
     return (
@@ -10,6 +11,7 @@ function Tempo(props) {
 }
 
 export function getStaticProps() {
+    console.log('> Passando pelo getStaticProps();')
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
     return {
