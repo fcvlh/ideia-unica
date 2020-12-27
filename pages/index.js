f4bb1bad5a2c476e028b746d7caad155087b2c6a
fcvlh <felipecarvalho4@hotmail.com>
2020-12-27 19:24:25 -0300

    Adição dos links de redes sociais. Facebook e Instagram

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
+import Img from 'next/image';
 
 function Home() {
     return (
@@ -8,6 +9,20 @@ function Home() {
             <Link href="/sobre">
                 <a>Acessar página Sobre</a>
             </Link>
+            &nbsp;|&nbsp;
+            <Link href="https://www.facebook.com/PapelSonhoEArte/">
+                <a>Facebook</a>
+            </Link>
+            &nbsp;|&nbsp;
+            <Link href="https://www.instagram.com/PapelSonhoEArte/">
+                <a>Instagram</a>
+            </Link>
+
+            {/* <Img
+                layout="fill"
+                src="https://scontent.fcgh16-1.fna.fbcdn.net/v/t1.0-9/102703410_100994004989415_21998834161851795_o.jpg?_nc_cat=110&ccb=2&_nc_sid=6e5ad9&_nc_ohc=hG64Avl5B_IAX9hpYQh&_nc_ht=scontent.fcgh16-1.fna&oh=6fd3dd7477b30e6a1be1268f5eb3fe33&oe=60106E94"
+            /> */}
+
         </div>)
 }
 
