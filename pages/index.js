5a4fef3bfaf92d32d7d79af51d679977c7066526
fcvlh <felipecarvalho4@hotmail.com>
2020-12-27 22:25:59 -0300

    Adsense Teste

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
+import AdSense from 'react-adsense';
 
 function Home() {
     return (
         <div>
+            {/* <AdSense.Google
+                client="ca-pub-3278313672041597"
+                slot=""
+            >
+            </AdSense.Google> */}
             <Head>
                 <script
                     data-ad-client="ca-pub-3278313672041597"
@@ -29,7 +35,7 @@ function Home() {
                 layout="fill"
                 src="https://scontent.fcgh16-1.fna.fbcdn.net/v/t1.0-9/102703410_100994004989415_21998834161851795_o.jpg?_nc_cat=110&ccb=2&_nc_sid=6e5ad9&_nc_ohc=hG64Avl5B_IAX9hpYQh&_nc_ht=scontent.fcgh16-1.fna&oh=6fd3dd7477b30e6a1be1268f5eb3fe33&oe=60106E94"
             /> */}
-
+            {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> */}
         </div>)
 }
 
